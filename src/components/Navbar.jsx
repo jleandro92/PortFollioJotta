6c91be9611462a31d6d64244659371caf7cbe629
Jotta <jleandro0802@gmail.com>
2026-06-04 18:10:49 -0300

    versão 1

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,7 +6,7 @@ export default function Navbar() {
       <nav className="max-w-7xl mx-auto px-6 h-20 flex items-center justify-between">
 
         <h1 className="font-black text-xl">
-          <span className="text-green-400">J</span>OTTA
+          <a href="#home" className="text-green-400">J</a>OTTA
         </h1>
 
         <div className="hidden md:flex gap-8 text-slate-300">
